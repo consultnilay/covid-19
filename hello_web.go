a0fc3d4bd22a12482bf89c3b29af58aca5d4c3f5
Nilay Agarwal <nilay.a@myparkplus.com>
2020-04-26 12:02:07 +0530

    Covid Stats over localhost browser

--- hello_web.go
+++ hello_web.go
@@ -3,6 +3,8 @@ package main
 import (
     "fmt"
     "net/http"
+    "log"
+    "io/ioutil"
 )
 
 func main() {
@@ -11,5 +13,18 @@ func main() {
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+/*    fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:]) */
+        resp, err := http.Get("https://thevirustracker.com/free-api?global=stats")
+        if err != nil {
+                log.Fatal(err)
+        }
+        if resp.StatusCode == http.StatusOK {
+                bodyBytes, err := ioutil.ReadAll(resp.Body)
+                if err != nil {
+                        log.Fatal(err)
+                }
+                bodyString := string(bodyBytes)
+                fmt.Fprintf(w, "%s", bodyString)
+        }
+        defer resp.Body.Close()
 }
